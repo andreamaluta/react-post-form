5e0dcd1b414a7e72635ace678daeeb4b7e91bb91
andreamaluta <andreamaluta@gmail.com>
2025-05-14 15:17:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import PostForm from './components/PostForm'
+
+function App() {
+
+
+  return (
+    <>
+      <PostForm />
+    </>
+  )
+}
+
+export default App
