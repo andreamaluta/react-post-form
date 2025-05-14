9bb0096a1f4384913432ba02fb7b1fabd16b47f5
andreamaluta <andreamaluta@gmail.com>
2025-05-14 15:33:41 +0200

    [UPD] made the form structure

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,7 +3,35 @@ import React from 'react'
 const PostForm = () => {
     return (
         <div>
-            <h1>hello world</h1>
+            <div className="container mt-4">
+                <div className="row">
+                    <form action="" className='border rounded bg-light p-4'>
+                        <div className="col-12">
+                            <input type="text" name='author' placeholder='Autore' className='form-control' />
+                        </div>
+                        <div className="col-12">
+                            <input type="text" name='title' placeholder='Titolo' className='form-control' />
+                        </div>
+                        <div className="col-12">
+                            <textarea name="body" id="" placeholder='Testo del post' className='form-control'></textarea>
+                        </div>
+                        <div className="col-12">
+                            <div>
+                                <input type="radio" id='privacyPublic' className='form-check-input mx-2' />
+                                <label htmlFor="privacyPublic" className="form-check-label"> Pubblico </label>
+                            </div>
+                            <div>
+                                <input type="radio" id='privacyPrivate' className='form-check-input mx-2' />
+                                <label htmlFor="privacyPrivate" className="form-check-label"> Bozza </label>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <button className='btn btn-primary'>Invia</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
     )
 }
